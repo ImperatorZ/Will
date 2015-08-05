7889626837d3d2a60557ebc5b83d096739bbc4f0
Imperator <asassins7770@gmail.com>
2015-08-05 18:59:48 +0300

    Test

--- app/src/main/java/team/will/Authorization.java
+++ app/src/main/java/team/will/Authorization.java
@@ -7,58 +7,53 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * Created by asass on 04.08.2015.
- */
 public class Authorization extends Activity {
 
-    private EditText editName;
-    private EditText editPass;
-    private EditText editTelephone;
+    private EditText mNameReg;
+    private EditText mPassReg;
+    private EditText mTelephoneReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.authorization_layout);
 
-        editName = (EditText) findViewById(R.id.editTextName);
-        editPass = (EditText) findViewById(R.id.editTextPassword);
-        editTelephone = (EditText) findViewById(R.id.editTextTelephone);
+        mNameReg = (EditText) findViewById(R.id.editTextName);
+        mPassReg = (EditText) findViewById(R.id.editTextPassword);
+        mTelephoneReg = (EditText) findViewById(R.id.editTextTelephone);
 
     }
 
 
     public void goAuth(View view) {
-        if (editName.getText().length() == 0) {
+        if(mNameReg.getText().length() == 0) {
             Toast.makeText(Authorization.this, "Введите имя", Toast.LENGTH_SHORT).show();
-        } else {
-            if (editPass.getText().length() == 0) {
+        }else{
+            if(mPassReg.getText().length() == 0) {
                 Toast.makeText(Authorization.this, "Введите пароль", Toast.LENGTH_SHORT).show();
-            } else {
-                if (editTelephone.getText().length() == 0) {
-                    Toast.makeText(Authorization.this, "Введите телефоне", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-
-
-        if (editName.getText().toString() == "Imperator") {
-        } else {
-            if (editPass.getText().toString() == "6310") {
-            } else {
-                if (editTelephone.getText().toString() == "79615223485"){
+            }else{
+                if(mTelephoneReg.getText().length() == 12) {
+                    Toast.makeText(Authorization.this, "Введите номер телефона", Toast.LENGTH_SHORT).show();
                 }else{
-                    Toast.makeText(Authorization.this, "Введенные данные не верны", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(this, MainActivityAuthorization.class);
+                    if(mTelephoneReg.getText().length() > 12){
+                        Toast.makeText(Authorization.this, "Вы не верно указали номер телефона", Toast.LENGTH_SHORT).show();
+                    }
+
+                    Intent intent = new Intent(this, Converter.class);
                     startActivity(intent);
-                    finish();
-                    setExitSharedElementCallback();
+                    Toast.makeText(Authorization. this, "Вы успешно зарегестрирвоались", Toast.LENGTH_SHORT).show();
 
                 }
+
             }
+
+
         }
     }
-}
+
+
+    }
+
 
 
 
